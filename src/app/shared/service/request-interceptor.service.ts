86eff46506161ffbb303fe36e4a166fd7969f6a8
Benja <Benjamin.Wilson24@gmail.com>
2018-10-16 13:06:13 -0400

    header check

--- src/app/shared/service/request-interceptor.service.ts
+++ src/app/shared/service/request-interceptor.service.ts
@@ -13,13 +13,15 @@ export class RequestInterceptorService implements HttpInterceptor {
   constructor() {}
 
   intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
-    // if (request.url.includes("/api/")) {
-    //   let jsonServerUrl = request.url.replace("/api/", "/bgw7/ng-redux/");
-    // request = request.clone({
-    //   url: `http://my-json-server.typicode.com${jsonServerUrl}`
-    // });
-    // console.log(request);
-    // }
+    if (request.headers.get('server')) {
+      if (request.url.includes('/api/') && request.headers.get('server').includes('GitHub.com')) {
+        let jsonServerUrl = request.url.replace('/api/', '/bgw7/ng-redux/');
+      request = request.clone({
+        url: `http://my-json-server.typicode.com${jsonServerUrl}`
+      });
+      console.log(request);
+      }
+    }
     return next.handle(request);
   }
 }

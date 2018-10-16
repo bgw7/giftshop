3157a85b5459ba905286127e4b14fd2d47fe8a1f
Benja <Benjamin.Wilson24@gmail.com>
2018-10-16 12:51:34 -0400

    comment

--- src/app/shared/service/request-interceptor.service.ts
+++ src/app/shared/service/request-interceptor.service.ts
@@ -13,13 +13,13 @@ export class RequestInterceptorService implements HttpInterceptor {
   constructor() {}
 
   intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
-    if (request.url.includes("/api/")) {
-      let jsonServerUrl = request.url.replace("/api/", "/bgw7/ng-redux/");
-    request = request.clone({
-      url: `http://my-json-server.typicode.com${jsonServerUrl}`
-    });
-    console.log(request);
-    }
+    // if (request.url.includes("/api/")) {
+    //   let jsonServerUrl = request.url.replace("/api/", "/bgw7/ng-redux/");
+    // request = request.clone({
+    //   url: `http://my-json-server.typicode.com${jsonServerUrl}`
+    // });
+    // console.log(request);
+    // }
     return next.handle(request);
   }
 }

24cbc443f3ec9e800fcfbbbd694cd4b345531353
Benja <Benjamin.Wilson24@gmail.com>
2018-10-19 15:25:02 -0400

    wip model and chef service

--- src/app/shared/model/Chef.ts
+++ src/app/shared/model/Chef.ts
@@ -1,4 +1,7 @@
+import { ChefIngredient } from "./Ingredient";
+
 export interface Chef {
     id: number;
     name: string;
+    inventory: ChefIngredient[]
 }
---
+++ src/app/shared/model/Ingredient.ts
@@ -0,0 +1,10 @@
+export interface Ingredient {
+    id: number,
+    name: string
+}
+
+export interface ChefIngredient extends Ingredient {
+    id: number;
+    name: string;
+    quantity: number;
+}
